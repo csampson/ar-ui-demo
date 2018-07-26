d56fbb2f4474aff0344eeaed4946921f4c4114c6
Cameron Sampson <csampson.js@gmail.com>
2018-07-26 11:59:07 -0500

    Scaffolding

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Entity, Scene } from 'aframe-react'
+
+const App = () => (
+  <Scene></Scene>
+)
+
+ReactDOM.render(<App />, document.getElementById('app'))
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  }
+}
