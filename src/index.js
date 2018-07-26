abb71214bede9ab6bafa3ee313d9734cdcf11cfa
Cameron Sampson <csampson.js@gmail.com>
2018-07-26 16:52:01 -0500

    Basic text panel

--- src/index.js
+++ src/index.js
@@ -2,22 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Entity, Scene } from 'aframe-react'
 
-const Panel = ({ children, position }) => (
+const Panel = ({ children, position = { x: 0, y: 1.5, z: -1 } }) => (
   <Entity geometry={{ primitive: 'plane' }} material={{ color: 'steelblue', metalness: 0.25 }} position={position}>
     {children}
   </Entity>
 )
 
 const Text = ({ children }) => (
-  <Entity text={{ align: 'center', width: 3, value: children }} />
+  <Entity text={{ align: 'center', width: 1, value: children }} />
 )
 
 const App = () => (
-  <Scene>
-    <Panel position={{ x: 0, y: 1.5, z: -1 }}>
-      <Text>o hey</Text>
-    </Panel>
-  </Scene>
+  <Panel>
+    <Text>o hey</Text>
+  </Panel>
 )
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.querySelector('#scene'))
